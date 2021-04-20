9c1e1b312741dbdb101a050ee03d44ead474ad95
dahlbergba <dahlberg.ba@gmail.com>
2021-04-20 19:50:43 -0400

    Changes to Dt, trial length, new FF
    New FF is FinalDistanceNoCrash.

--- run.py
+++ run.py
@@ -7,14 +7,14 @@ import time
 
 
 # TASK PARAMETERS ---------------------------------------------------------------
-Dt = 0.01
+Dt = 0.1
 #target_size = [45, 55, 65, 75]
 #initial_distance = [120, 135, 150, 165, 180, 205, 210]
 #initial_velocity = [10, 11, 12, 13, 14, 15]
 target_size = [55, 65]
 initial_distance = [150, 165, 180]
 initial_velocity = [12, 13]
-trial_length = 10 # (sec)
+trial_length = 30 # (sec)
 optical_variable = 0
 trials = len(target_size) * len(initial_distance) * len(initial_velocity) 
 
@@ -30,14 +30,14 @@ GenotypeLength = Size*Size + Size*3
 
 # EA PARAMETERS -----------------------------------------------------------------
 GenotypeLength = Size*Size + Size*3    # Slightly longer because of incoding the input weight vector
-Population = 5
+Population = 10
 RecombProb = 0.5
 MutatProb = 0.1
-Generations = 10
+Generations = 25
 Tournaments = Generations * Population
 
-# FITNESS FUNCTIONS -------------------------------------------------------------
 
+# FITNESS FUNCTIONS -------------------------------------------------------------
 def fitnessFunction1(genotype):
     """First version of the fitness function. Here we evolve agents simply on their ability to end 
     the trial near the target - crashes count, too!"""
@@ -60,21 +60,51 @@ def fitnessFunction1(genotype):
                 final_distances[i] = agent.Distance
                 i += 1
 
-    return np.average(final_distances)
+    return -1 * np.average(final_distances)
+
+
+def fitnessFunction2(genotype):
+    """First version of the fitness function. Here we evolve agents simply on their ability to end 
+    the trial near the target - crashes count, too!"""
+    
+    final_distances = np.empty(trials)
+    i = 0
+    
+    agent = AgentEnv(genotype, Size, WeightRange, BiasRange, TimeConstMin, TimeConstMax, InputWeightRange, Dt)
+    for ts in target_size:
+        for d in initial_distance:
+            for v in initial_velocity:  
+                agent.setInitialState(v, d, ts)
+                agent.Optical_variable = optical_variable
+                # While agent hasn't crashed, agent is still moving forward significantly, and not too much time has elapsed
+                while (agent.Distance > 0) and (agent.Velocity > 0.005) and (agent.Time < trial_length):
+                    agent.sense()
+                    agent.think()
+                    agent.act()
+    
+                if agent.Distance < 0:   # if agent crashed, reset distance to starting position
+                    agent.Distance = 180
+                    
+                final_distances[i] = agent.Distance/d   # fitness is proportion of distance
+                i += 1
+
+    return -1 * np.average(final_distances)
 
 
 # ============================= RUNTIME ========================================
-    
+
+# Set up    
 start = time.time()
 #np.random.seed(2)
 print('Number of Evaluation Trials: %i' % trials)    
 
-population = Microbial(fitnessFunction1, Population, GenotypeLength, RecombProb, MutatProb)
+# Run evolutionary simulation 
+population = Microbial(fitnessFunction2, Population, GenotypeLength, RecombProb, MutatProb)
 population.run(Tournaments)
-filename = 'FinalDistance_G%i_V%i_%s' % ( int(population.generationsRun), optical_variable, population.dateCreated )
+filename = 'FinalDistanceNoCrash2_G%i_V%i_%s' % ( int(population.generationsRun), optical_variable, population.dateCreated )
 save(filename, population)
 
+# Show graphs and save 
 population.showFitnessSummary(('%s_Summary.png' % filename))
-population.showFitnessTrajectories(('%s_Trajectories.png' % filename), _alpha=0.1)
-       
+population.showFitnessTrajectories(('%s_Trajectories.png' % filename), _alpha=0.1)       
 print('TOTAL TIME ELAPSED: %i sec' % (int(time.time()-start)))
