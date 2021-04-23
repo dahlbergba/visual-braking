f2f3be51521e0343f41f1e0b7f0302378888d505
dahlbergba <dahlberg.ba@gmail.com>
2021-04-23 18:04:17 -0400

    Added new runEndless() method, subdivided run() into tournament() and runTournaments()

--- mga.py
+++ mga.py
@@ -2,6 +2,7 @@ import random
 import numpy as np
 import matplotlib.pyplot as plt
 from datetime import date
+from tools import save
 import time
 
 class Microbial():
@@ -31,7 +32,6 @@ class Microbial():
         if savename !='':
             plt.savefig(savename)
         plt.show()
-
             
             
     def showFitnessTrajectories(self, savename='', _alpha=0.1):
@@ -46,74 +46,56 @@ class Microbial():
             plt.savefig(savename)
         plt.show()
 
-            
-            
+                        
     def fitStats(self):
-#        bestfit = self.fitnessFunction(self.pop[0])
-#        bestind = -1
-#        avgfit = 0
-#        for i in self.pop:
-#            fit = self.fitnessFunction(i)
-#            avgfit += fit
-#            if (fit > bestfit):
-#                bestfit = fit
-#                bestind = i
-#         return avgfit/self.popsize, bestfit, popfit, bestind
         popfit = np.zeros((self.pop.shape[0])) # Vector of population fitnesses
         for i in range(self.pop.shape[0]):
             fitness = self.fitnessFunction(self.pop[i])
             popfit[i] = fitness
         
-        bi = popfit.argmax()
-        bg = self.pop[bi]
-        
+        bi = popfit.argmax()    # best individual
+        bg = self.pop[bi]       # best genome
         return popfit.mean(), popfit.max(), popfit.std(), bi, bg, popfit
 
 
-    def run(self, tournaments, report=True):
+    def tournament(self):
 
-        # Evolutionary loop
+        # Step 1: Pick 2 individuals
+        a = random.randint(0,self.popsize-1)
+        b = random.randint(0,self.popsize-1)
+        while (a==b):   # Make sure they are two different individuals
+            b = random.randint(0,self.popsize-1)
+
+        # Step 2: Compare their fitness
+        if (self.fitnessFunction(self.pop[a]) > self.fitnessFunction(self.pop[b])):
+            winner = a
+            loser = b
+        else:
+            winner = b
+            loser = a
+
+        # Step 3: Transfect loser with winner
+        for l in range(self.genesize):
+            if (random.random() < self.recombProb):
+                self.pop[loser][l] = self.pop[winner][l]
+
+        # Step 4: Mutate loser and Make sure new organism stays within bounds
+        for l in range(self.genesize):
+            self.pop[loser][l] += random.gauss(0.0,self.mutatProb)
+            if self.pop[loser][l] > 1.0:
+                self.pop[loser][l] = 1.0
+            if self.pop[loser][l] < -1.0:
+                self.pop[loser][l] = -1.0
+
+
+    def runTournaments(self, tournaments, report=True):
         start = time.time()
         report_progress = 0
+        
+        # Evolutionary loop
         for i in range(tournaments):
-
-            # Report statistics every generation
-            if (i%self.popsize==0):
-                #print(i/self.popsize)
-                #af, bf, bi = self.fitStats()
-                af, bf, sd, bi, bg, pf = self.fitStats()
-                self.avgHistory.append(af)
-                self.bestHistory.append(bf)
-                self.popHistory.append(pf)
-
-            # Step 1: Pick 2 individuals
-            a = random.randint(0,self.popsize-1)
-            b = random.randint(0,self.popsize-1)
-            while (a==b):   # Make sure they are two different individuals
-                b = random.randint(0,self.popsize-1)
-
-            # Step 2: Compare their fitness
-            if (self.fitnessFunction(self.pop[a]) > self.fitnessFunction(self.pop[b])):
-                winner = a
-                loser = b
-            else:
-                winner = b
-                loser = a
-
-            # Step 3: Transfect loser with winner
-            for l in range(self.genesize):
-                if (random.random() < self.recombProb):
-                    self.pop[loser][l] = self.pop[winner][l]
-
-            # Step 4: Mutate loser and Make sure new organism stays within bounds
-            for l in range(self.genesize):
-                self.pop[loser][l] += random.gauss(0.0,self.mutatProb)
-                if self.pop[loser][l] > 1.0:
-                    self.pop[loser][l] = 1.0
-                if self.pop[loser][l] < -1.0:
-                    self.pop[loser][l] = -1.0
-
-            if report==True:    # Print status updates to consode
+            self.tournament()
+            if report==True:    # Print status updates to console, if enabled
                 if i/tournaments*100 > report_progress:
                     print(' \n%d%% Complete' % (report_progress))
                     print('Fitness Mean=%f, Max=%f, SD=%f' % tuple(self.fitStats()[0:3]))
@@ -123,4 +105,23 @@ class Microbial():
         # Data integrity stuff
         self.generationsRun += (tournaments/self.popsize)
         self.dateEdited = str(date.today())
+        
+        
+    def runEndless(self, filename, interval=15):   # Interval = minutes between saves and reports
+        start = time.time()
+        last_report = start
+        # Evolutionary loop
+        while True:     #TODO: Modify this to detect some key press
+            self.tournament()
+            if (time.time()-last_report) > (interval*60):     # If it's been more than 15 minutes since last report
+                print("Saving...")
+                save(filename, self)
+                print(' \nGenerations Run: %f' % self.generationsRun)     # Print generations run so far
+                print('Fitness Mean=%f, Max=%f, SD=%f' % tuple(self.fitStats()[0:3]))
+                print('Time elapsed: %f sec / %f min / %f hours' % ( (time.time()-start), (time.time()-start)/60, (time.time()-start)/3600 )) 
+                last_report = time.time()
+
+
+
+        
         
